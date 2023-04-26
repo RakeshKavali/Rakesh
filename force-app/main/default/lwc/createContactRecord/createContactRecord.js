b3069e61a6d0fec5ca03b4107457379a517d4137
RakeshKavali <rakeshmudhiraj20@gmail.com>
2023-04-26 12:08:49 -0400

    record forms

--- force-app/main/default/lwc/createContactRecord/createContactRecord.js
+++ force-app/main/default/lwc/createContactRecord/createContactRecord.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.lastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.email';
+import PHONE_FIELD from '@salesforce/schema/Contact.phone';
+export default class CreateContactRecord extends LightningElement {
+    objectApiName = CONTACT_OBJECT;
+    fields =[FIRSTNAME_FIELD, LASTNAME_FIELD, EMAIL_FIELD, PHONE_FIELD];
+    handleSuccess(event){
+        const toastEvent = new ShowToastEvent({
+            title:"Account has been created successfully !",
+            message: "Account Created ",
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
 
-export default class CreateContactRecord extends LightningElement {}
+    
+    }
